b1ccbcbf2f65c2a15dd5158cfbda10348c32c02a
Daniel Dodge <dandantheman87@gmail.com>
2021-11-12 09:29:21 -0700

    finsihed sldc section

---
+++ sldc.js
@@ -0,0 +1,26 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Planning
+// Analysis of Requirements
+// Design
+// Implementation
+// Testing & Integration
+// Maintain
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+*/
+
+let planning = "Planning and outlining how the code will be written"
+let analysisOfRequirements = "Evaluate what is needed to properly go through with the project"
+let design = "Giving the code a more user-friendly interface"
+let implementation = "Implement the software into its respective environment"
+let testingAndIntegration = "Checking for bugs and problems until the final product is done"
+let maintain = "Perform maintenance for any problems that arise after the project becomes public"
