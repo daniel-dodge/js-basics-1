9d8c96e2b2e7a0fe3ab6f457a1d00c7200b35cd9
Daniel Dodge <dandantheman87@gmail.com>
2021-11-12 11:27:42 -0700

    finished password

--- password.js
+++ password.js
@@ -1,17 +1,26 @@
-console.log("Welcome to the Password Validator tool")
-
 const { strictEqual } = require('assert')
 const readline = require('readline')
 const reader = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
-
-reader.question("What is your password", function(answer){
-    
-    if (answer.length > 10) {
-        console.log("thats a goood password")
-    } else {
-        console.log("you def getting hacked")
-    }
+console.log('| |_ ___| | |___ \n' +
+`${"|   | -_| | | . |"}\n`   + `${"|_|_|___|_|_|___|"}`);
+console.log("Welcome to the Password Validator tool")
+reader.question(" Press enter to start  ", function(answer){
+    console.log(' _____ \n' +
+    `${"|___  |"}\n`   + `${"  |  _|"}\n` + `${"  |_|  "}\n` + `${"  |_|  "}\n`)
+    reader.question("What is your password?   ", function(answer){
+        
+        if (answer.length >= 10) {
+            console.log(' ___|_|___ ___ \n' +
+            `${"|   | |  _| -_|"}\n`   + `${"|_|_|_|___|___|"}`);
+            
+            console.log("thats a goood password")
+        } else {
+            console.log('               _| \n' +
+            `${" _|          _|    "}\n`   + `${"   _|    _|  _|    "}\n` + `${"     _|      _|    "}\n` + `${"   _|        _|    "}\n`+ `${" _|      _|  _|    "}\n    `+ `${"           _|  "}\n`)
+            console.log("you def getting hacked")
+        }
+    })
 })
